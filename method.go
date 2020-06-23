9545c8c00c0dc64175a28457aefe05b012215ebb
henrylee2cn <henrylee2cn@gmail.com>
2020-06-23 21:59:08 +0800

    feat: add method
    Change-Id: Iae3af02878eae528faafaa3a01bf5ca101fba149

---
+++ method.go
@@ -0,0 +1,21 @@
+package rester
+
+import (
+	"strings"
+
+	"github.com/henrylee2cn/ameda"
+)
+
+var httpMethodList = []string{
+	"Any", "GET", "POST", "PUT", "PATCH", "HEAD", "OPTIONS", "DELETE", "CONNECT", "TRACE",
+	"CORS_GET", "CORS_POST", "CORS_PUT", "CORS_PATCH", "CORS_HEAD", "CORS_DELETE", "CORS_TRACE",
+}
+
+func splitMethod(ctlMethod string) (httpMethod string, cors bool) {
+	if !ameda.StringsIncludes(httpMethodList, ctlMethod) {
+		return
+	}
+	httpMethod = strings.TrimPrefix(ctlMethod, "CORS_")
+	cors = httpMethod != ctlMethod
+	return
+}
